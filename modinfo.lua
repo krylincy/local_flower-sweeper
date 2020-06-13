c14d37b993e88b54f903b64dc211000be419ebc3
Corinna Bruchwald <51796518+krylincy@users.noreply.github.com>
2020-06-13 21:50:43 +0200

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,32 @@
+name = "Flower Sweeper"
+version = "1.0"
+description = "You can change the flowertype with the Clean Sweeper.\n\nChange the chance to spawn a rose intstead of a regular flower in the config. This only apply to the chance when sweeping a flower, not when planting a butterfly. \n\nVersion: "..version
+author = "krylincy"
+api_version = 10
+forumthread = ""
+icon_atlas = "preview-flower.xml"
+icon = "preview-flower.tex"
+dst_compatible = true
+client_only_mod = false
+all_clients_require_mod = true
+
+configuration_options = {
+	{
+		name = "rosePercent",
+		label = "Rose Chance",
+        hover = "The chance to spawn a rose instead of regular flower. Game default is 1%.",
+		options = {
+			{description = "1%", data = 0.01},
+			{description = "2%", data = 0.02},
+			{description = "3%", data = 0.03},
+			{description = "4%", data = 0.04},
+			{description = "5%", data = 0.05},
+			{description = "6%", data = 0.06},
+			{description = "7%", data = 0.07},
+			{description = "8%", data = 0.08},
+			{description = "9%", data = 0.09},
+			{description = "10%", data = 0.1},
+		},
+		default = 0.01,
+	},
+}
---
+++ modmain.lua
@@ -0,0 +1,44 @@
+
+AddPrefabPostInit("reskin_tool", function(inst) 
+	if inst.components ~= nil and inst.components.spellcaster ~= nil then
+		-- save old functions to just extend them
+		local oldSpellFunction = inst.components.spellcaster.spell			
+		local oldTestSpellFunction = inst.components.spellcaster.can_cast_fn			
+		
+		
+		local function can_cast_fn(doer, target, pos)	
+			-- if it is a flower, return true, else return whatever the default function would return
+			return target.prefab == "flower" or oldTestSpellFunction(doer, target, pos)
+		end
+		
+		local function spellCB(tool, target, pos)					
+			if target.prefab == "flower" then
+				local names = {"f1","f2","f3","f4","f5","f6","f7","f8","f9","f10"}
+				local ROSE_NAME = "rose"
+				local ROSE_CHANCE = GetModConfigData("rosePercent")		
+
+
+				-- the puff effect
+				local fx = GLOBAL.SpawnPrefab("explode_reskin")
+				fx.Transform:SetScale(1, 1, 1)
+
+				local fx_pos_x, fx_pos_y, fx_pos_z = target.Transform:GetWorldPosition()
+				fx.Transform:SetPosition(fx_pos_x, fx_pos_y, fx_pos_z) 
+				
+				-- change flower skin as in the flower "setflowertype" function
+				target.animname = math.random() < ROSE_CHANCE and ROSE_NAME or names[math.random(#names)]
+				target.AnimState:PlayAnimation(target.animname)
+				if target.animname == ROSE_NAME then
+					target:AddTag("thorny")
+				end		
+			else
+				-- do default stuff
+				oldSpellFunction(tool, target, pos)
+			end				
+		end	
+		
+		inst.components.spellcaster:SetSpellFn(spellCB)
+		inst.components.spellcaster:SetCanCastFn(can_cast_fn)
+	end
+
+end)
